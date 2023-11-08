abc75df498e3712d15ecb53eec48423dad39bea8
YehorBondarenko96 <135607282+YehorBondarenko96@users.noreply.github.com>
2023-11-08 12:14:40 +0000

    Deploying to gh-pages from @ YehorBondarenko96/English-children-shop@37317f4bef37aebe9d930f73cf58bc723306a3e7 🚀

---
+++ index.13b45a53.js
@@ -0,0 +1,2 @@
+var listButtonMenu=document.querySelector(".list-button-menu"),buttonMenu=document.querySelector(".button-menu");buttonMenu.addEventListener("click",(function(){listButtonMenu.classList.add("button-menu-focus")})),buttonMenu.addEventListener("blur",(function(){setTimeout((function(){listButtonMenu.classList.remove("button-menu-focus")}),0)}));
+//# sourceMappingURL=index.13b45a53.js.map
