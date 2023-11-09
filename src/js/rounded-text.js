236a00cd985ea6839a08b10908652b6d91dddef0
YehorBondarenko96 <135607282+YehorBondarenko96@users.noreply.github.com>
2023-11-09 18:39:50 +0200

    8

--- src/js/rounded-text.js
+++ src/js/rounded-text.js
@@ -1,13 +1,17 @@
-const lettersMenuHeader = document.querySelectorAll('.letter-menu-headaer');
+const lettersMenuHeader = document.querySelectorAll('.header-menu-home');
+const headerMenuForLessonsOne = document.querySelectorAll('.header-menu-for-lessons-one');
+const headerMenuForLessonsTwo = document.querySelectorAll('.header-menu-for-lessons-two')
 
 
 
-function rotateLetter(value){
-    let deg = (lettersMenuHeader.length-0.5)/2*(-value);
-    for(let item of lettersMenuHeader){
+function rotateLetter(value, massif){
+    let deg = (massif.length-0.5)/2*(-value);
+    for(let item of massif){
         item.style.transform = `rotate(${deg}deg)`;
         deg += value
     }
 };
 
-rotateLetter(-10);
+rotateLetter(-10, lettersMenuHeader);
+rotateLetter(-8, headerMenuForLessonsOne);
+rotateLetter(-6.5, headerMenuForLessonsTwo);
