6af3e3bdb8124cc0478b9656e23af1232a239816
YehorBondarenko96 <135607282+YehorBondarenko96@users.noreply.github.com>
2023-11-09 18:10:04 +0200

    7

--- src/js/rounded-text.js
+++ src/js/rounded-text.js
@@ -3,12 +3,11 @@ const lettersMenuHeader = document.querySelectorAll('.letter-menu-headaer');
 
 
 function rotateLetter(value){
-    let deg = (lettersMenuHeader.length - 1)/2*(-value);
-    console.log(deg);
+    let deg = (lettersMenuHeader.length-0.5)/2*(-value);
     for(let item of lettersMenuHeader){
         item.style.transform = `rotate(${deg}deg)`;
         deg += value
     }
 };
 
-rotateLetter(-30);
+rotateLetter(-10);
