4cde32047d5dd2207f94afd63b3980b526f1c628
YehorBondarenko96 <135607282+YehorBondarenko96@users.noreply.github.com>
2023-11-09 14:43:34 +0000

    Deploying to gh-pages from @ YehorBondarenko96/English-children-shop@c6109d757580452867956aba1a2cafc352f9807c 🚀

---
+++ index.63608f9e.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector(".list-button-menu"),e=document.querySelector(".button-menu");e.addEventListener("click",(function(){t.classList.add("button-menu-focus")})),e.addEventListener("blur",(function(){setTimeout((function(){t.classList.remove("button-menu-focus")}),0)}))}();
+//# sourceMappingURL=index.63608f9e.js.map
