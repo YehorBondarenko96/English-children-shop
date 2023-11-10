f275d67eaa71b408e7853060e32a425283c98759
YehorBondarenko96 <135607282+YehorBondarenko96@users.noreply.github.com>
2023-11-10 11:34:33 +0200

    5

--- src/js/my-products.js
+++ src/js/my-products.js
@@ -1,68 +1,74 @@
-import { galleryItems } from "./gallety-my-products";
+// import { async } from "@vimeo/player";
+// import { galleryItems } from "./gallety-my-products";
 
-const main = document.querySelector('main');
-const itemsButtonMenu = document.querySelectorAll('.item-button-menu');
+// const main = document.querySelector('main');
 
-let idItemButtonMenu = '';
-let buttonsGallery = '';
-let modalGallery = '';
+// async function definesItemsButtonMenu() {
+//     const itemsButtonMenu = await document.querySelectorAll('.item-button-menu');
+//     [...itemsButtonMenu].forEach((itemButtonMenu) => {
+//         console.log(1);
+//         itemButtonMenu.addEventListener('click', renderMainForProducts)
+//         });
+// };
+// definesItemsButtonMenu();
 
-const removeVisuallyHidden = () => {
-    modalGallery.classList.remove('visually-hidden');
-    renderItemsVideo(event, galleryItems);
-};
-const addVisuallyHidden = () => {
-    modalGallery.classList.add('visually-hidden');
-    modalGallery.innerHTML = ''
-};
-const removeEventListenerModalGallery = () => {addVisuallyHidden();
-    modalGallery.removeEventListener('click', removeEventListenerModalGallery)};
+// let idItemButtonMenu = '';
+
+// const removeVisuallyHidden = () => {
+//     modalGallery.classList.remove('visually-hidden');
+//     renderItemsVideo(event, galleryItems);
+// };
+// const addVisuallyHidden = () => {
+//     modalGallery.classList.add('visually-hidden');
+//     modalGallery.innerHTML = ''
+// };
+// const removeEventListenerModalGallery = () => {addVisuallyHidden();
+//     modalGallery.removeEventListener('click', removeEventListenerModalGallery)};
 
 
-function renderItemsVideo (event, galleryItems) {
-    const idItem = event.currentTarget.getAttribute('id');
-    const itemVideo = galleryItems[idItemButtonMenu].find(galleryItem => galleryItem.id == idItem).urlVideo;
-    modalGallery.innerHTML = `<iframe 
-    width="560" 
-    height="315" 
-    src="${itemVideo}" 
-    title="YouTube video player" 
-    frameborder="0" 
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-    allowfullscreen>
-</iframe>`;
-};
+// function renderItemsVideo (event, galleryItems) {
+//     const idItem = event.currentTarget.getAttribute('id');
+//     const itemVideo = galleryItems[idItemButtonMenu].find(galleryItem => galleryItem.id == idItem).urlVideo;
+//     modalGallery.innerHTML = `<iframe 
+//     width="560" 
+//     height="315" 
+//     src="${itemVideo}" 
+//     title="YouTube video player" 
+//     frameborder="0" 
+//     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+//     allowfullscreen>
+// </iframe>`;
+// };
 
-itemsButtonMenu.forEach((itemButtonMenu) => {
-itemButtonMenu.addEventListener('click', renderMainForProducts)
-});
 
-function renderMainForProducts(event) {
-    idItemButtonMenu = event.currentTarget.getAttribute('id');
-    const itemMainForProducts = galleryItems[idItemButtonMenu].map((galleryItem) =>
-        `<li>
-        <button id="${galleryItem.id}" class="button-gallery" type="button">
-        <img class="img-manual" src="${galleryItem.img}" alt="manual"/>
-        <h3 class="h3-manual">${galleryItem.name}<br> <span>${galleryItem.surname}</span></h3>
-        <p class="p-manual">${galleryItem.description}</p>
-        <p class="button-for-more">Click to learn more</p>
-    </button>
-    </li>`
-    ).join("");
-    const contentMain = `<ul class="gallery">
-        ${itemMainForProducts}
-</ul>
-<div class="modal-gallery visually-hidden">
-</div>`
-    main.innerHTML = contentMain;
-    buttonsGallery = document.querySelectorAll('.button-gallery')
+
+// function renderMainForProducts(event) {
+//     console.log(2);
+//     idItemButtonMenu = event.currentTarget.getAttribute('id');
+//     const itemMainForProducts = galleryItems[idItemButtonMenu].map((galleryItem) =>
+//         `<li>
+//         <button id="${galleryItem.id}" class="button-gallery" type="button">
+//         <img class="img-manual" src="${galleryItem.img}" alt="manual"/>
+//         <h3 class="h3-manual">${galleryItem.name}<br> <span>${galleryItem.surname}</span></h3>
+//         <p class="p-manual">${galleryItem.description}</p>
+//         <p class="button-for-more">Click to learn more</p>
+//     </button>
+//     </li>`
+//     ).join("");
+//     const contentMain = `<ul class="gallery">
+//         ${itemMainForProducts}
+// </ul>
+// <div class="modal-gallery visually-hidden">
+// </div>`
+//     main.innerHTML = contentMain;
+//     const buttonsGallery = document.querySelectorAll('.button-gallery')
     
 
-buttonsGallery.forEach((buttonGallery) => {
-    buttonGallery.addEventListener('click', () => {
-        modalGallery = document.querySelector('.modal-gallery');
-        removeVisuallyHidden();
-        modalGallery.addEventListener('click', removeEventListenerModalGallery)
-    });
-});
-};
+// buttonsGallery.forEach((buttonGallery) => {
+//     buttonGallery.addEventListener('click', () => {
+//         const modalGallery = document.querySelector('.modal-gallery');
+//         removeVisuallyHidden();
+//         modalGallery.addEventListener('click', removeEventListenerModalGallery)
+//     });
+// });
+// };
